622b46b1ea5bf85bcf1b956135d1433680ef7350
Manuel Vargas <manuelvargas@my.unt.edu>
2016-02-06 16:02:54 -0600

    Create acolor.js

---
+++ acolor.js
@@ -0,0 +1,25 @@
+
+var d = new Date();
+var n = d.getDay();
+  
+if (n == 0) {
+  document.linkColor = "#cc334d";
+    }
+if (n == 1) {
+  document.linkColor = "#ad33cc";
+  }
+if (n == 2) {
+  document.linkColor = "#337fcc";
+  }
+if (n == 3) {
+  document.linkColor = "#33ccc3";
+  }
+if (n == 4) {
+  document.linkColor = "#33cc57";
+  }
+if  (n == 5) {
+  document.linkColor = "#c7cc33";
+  }
+if (n == 6) {
+  document.linkColor = "#cc5733";
+  }
